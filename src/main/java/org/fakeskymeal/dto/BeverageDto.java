2f9aba33bb493fcced414a414d2efb16ee4be097
Bryan Lee <bryanlee56098@gmail.com>
2025-05-12 23:25:31 -0700

    BeverageDto

---
+++ src/main/java/org/fakeskymeal/dto/BeverageDto.java
@@ -0,0 +1,96 @@
+package org.fakeskymeal.dto;
+
+import java.time.LocalDate;
+
+public class BeverageDto extends BaseDto {
+    int beverageId;
+    int inventoryId;
+    String name;
+    String brand;
+    int quantity;
+    float weight;
+    LocalDate deliveredDate;
+    LocalDate expirationDate;
+    String description;
+
+    public BeverageDto() {
+        super();
+    }
+
+    @Override
+    public int getId() {
+        return beverageId;
+    }
+
+    public int getBeverageId() {
+        return beverageId;
+    }
+
+    public void setBeverageId(int beverageId) {
+        this.beverageId = beverageId;
+    }
+
+    public int getInventoryId() {
+        return inventoryId;
+    }
+
+    public void setInventoryId(int inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public LocalDate getDeliveredDate() {
+        return deliveredDate;
+    }
+
+    public void setDeliveredDate(LocalDate deliveredDate) {
+        this.deliveredDate = deliveredDate;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
